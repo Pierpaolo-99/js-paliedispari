c9435aea309009ac1fcfed0ceb98c09be7692a0c
Pierpaolo-99 <pierpaolodellioio@gmail.com>
2025-02-17 01:23:00 +0100

    add function generate random number

--- main.js
+++ main.js
@@ -2,13 +2,22 @@
 
 // domandare all'utente pari o dispari
 
-const = prompt('scegliere pari o dispari')
+const user_choice = prompt('scegliere pari o dispari')
 
 // domandare all'utente un numero da 1 a 5
 
-const = Number(prompt('scegliere un numero da 1 a 5'))
+const user_number = Number(prompt('scegliere un numero da 1 a 5'))
 
 // generazione numero random da 1 a 5 per il computer con funzione
+
+function generate_rnd_number(min, max){
+    return number = Math.floor(Math.random() * (max - min +1)) +min;
+}
+
+const computer_number = generate_rnd_number(1, 5);
+
+console.log(computer_number);
+
 
 // somma dei numeri con funzione
 
