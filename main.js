0d636be7fffd06bc9a9a0f68a29e57f99d198a6d
Pierpaolo-99 <pierpaolodellioio@gmail.com>
2025-02-17 01:06:09 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+/* traccia: L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto. */
+
+// domandare all'utente pari o dispari
+
+const = prompt('scegliere pari o dispari')
+
+// domandare all'utente un numero da 1 a 5
+
+const = Number(prompt('scegliere un numero da 1 a 5'))
+
+// generazione numero random da 1 a 5 per il computer con funzione
+
+// somma dei numeri con funzione
+
+// verifica pari o dispari con funzione
+
+// stampa del vincitore
